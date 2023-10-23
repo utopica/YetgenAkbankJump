12f432506610a7a03cf22365f1a3e2caec6da070
utopica <h.elifokumus@gmail.com>
2023-10-23 11:28:35 +0300

    Add project files.

---
+++ YetgenAkbankJump.WebApi/Controllers/CarsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using YetgenAkbankJump.WebApi.Data;
+
+namespace YetgenAkbankJump.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            //Gets all the cars
+            var cars = CarsContext.LuxuryCars.ToList();
+            return Ok(cars);
+
+            //if the operaction successfull(Ok()) return cars. 
+
+        }
+    }
+}
